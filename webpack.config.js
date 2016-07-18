1875acc829fa99a3845fe33f5c539bc301759591
Mark Polak <mark@thedutchies.com>
2016-07-18 23:52:11 +0200

    Simple pre render

--- webpack.config.js
+++ webpack.config.js
@@ -89,13 +89,13 @@ if (!isDevBuild) {
         }),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            //     compress: {
-            //         warnings: false,
-            //     },
-            comments: false,
-            beautify: true,
-        }),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     //     compress: {
+        //     //         warnings: false,
+        //     //     },
+        //     comments: false,
+        //     beautify: true,
+        // }),
     ];
 } else {
     webpackConfig.plugins = [
